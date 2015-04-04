a2f55843e5490d639f687571aaacefab47bd0a84
tanyawilke <darK20echo@git>
2015-04-04 19:55:21 +0200

    Amended soccerviz.js

--- D3/js/soccerviz.js
+++ D3/js/soccerviz.js
@@ -6,28 +6,30 @@
     function overallTeamViz(incomingData) {
         d3.select("svg")
         .append("g")
-        .attr("id", "teamsG")
-        .attr("transform", "translate(50,300)")
-        .selectAll("g")
-        .data(incomingData)
-        .enter()
-        .append("g")
-        .attr("class", "overallG")
-        .attr("transform",
-        function (d, i) { return "translate(" + (i * 50) + ", 0)" }
+            .attr("id", "teamsG")
+            .attr("transform", "translate(50,300)")
+            .selectAll("g")
+            .data(incomingData)
+            .enter()
+            .append("g")
+            .attr("class", "overallG")
+            .attr("transform",
+            function (d, i) { return "translate(" + (i * 50) + ", 0)" }
         );
+
         var teamG = d3.selectAll("g.overallG");
+
         teamG
-        .append("circle")
-        .attr("r", 20)
-        .style("fill", "pink")
-        .style("stroke", "black")
-        .style("stroke-width", "1px");
-        teamG
-        .append("text")
-        .style("text-anchor", "middle")
-        .attr("y", 30)
-        .style("font-size", "10px")
-        .text(function (d) { return d.team; });
+            .append("circle")
+            .attr("r", 20)
+            .style("fill", "pink")
+            .style("stroke", "black")
+            .style("stroke-width", "1px");
+            teamG
+            .append("text")
+            .style("text-anchor", "middle")
+            .attr("y", 30)
+            .style("font-size", "10px")
+            .text(function (d) { return d.team; });
     }
 }
