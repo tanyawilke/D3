f371eaa9267511def18199f4f54daecd0b4a59a2
tanyawilke <darK20echo@git>
2015-04-04 18:48:28 +0200

    New files

---
+++ D3/js/soccerviz.js
@@ -0,0 +1,33 @@
+﻿function createSoccerViz() {
+    d3.csv("worldcup.csv", function(data) {
+        overallTeamViz(data);
+    })
+
+    function overallTeamViz(incomingData) {
+        d3.select("svg")
+        .append("g")
+        .attr("id", "teamsG")
+        .attr("transform", "translate(50,300)")
+        .selectAll("g")
+        .data(incomingData)
+        .enter()
+        .append("g")
+        .attr("class", "overallG")
+        .attr("transform",
+        function (d, i) { return "translate(" + (i * 50) + ", 0)" }
+        );
+        var teamG = d3.selectAll("g.overallG");
+        teamG
+        .append("circle")
+        .attr("r", 20)
+        .style("fill", "pink")
+        .style("stroke", "black")
+        .style("stroke-width", "1px");
+        teamG
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", 30)
+        .style("font-size", "10px")
+        .text(function (d) { return d.team; });
+    }
+}
